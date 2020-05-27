89ba311d3dd996ffe5671719b594b2286f18bf65
asiri777 <asandaruwan64@gmail.com>
2020-05-27 10:43:12 +0530

    Add nic validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,25 +16,41 @@ export class AppComponent {
     let gender: string;
     let nicNo = this.nic;
 
-    nicNo = nicNo.length === 10 ? "19" + nicNo : nicNo;
+    if (this.validateNic(nicNo)) {
+      nicNo = nicNo.length === 10 ? "19" + nicNo : nicNo;
 
-    birthYear = +nicNo.substring(0, 4);
-    noOfDays = +nicNo.substring(4, 7);
+      birthYear = +nicNo.substring(0, 4);
+      noOfDays = +nicNo.substring(4, 7);
 
-    noOfDays = noOfDays > 500 ? noOfDays - 500 : noOfDays;
-    gender = noOfDays > 500 ? "F" : "M";
+      noOfDays = noOfDays > 500 ? noOfDays - 500 : noOfDays;
+      gender = noOfDays > 500 ? "F" : "M";
 
-    if (!this.checkLeapYear(birthYear) && noOfDays > 60) {
-      noOfDays -= 1;
+      if (!this.checkLeapYear(birthYear) && noOfDays > 60) {
+        noOfDays -= 1;
+      }
+
+      tempBirthDay = new Date(`${birthYear}-01-01`);
+      tempBirthDay.setDate(noOfDays);
+
+      this.birthday = tempBirthDay.toDateString();
+    } else {
+      this.birthday = "Invalid";
     }
-
-    tempBirthDay = new Date(`${birthYear}-01-01`);
-    tempBirthDay.setDate(noOfDays);
-
-    this.birthday = tempBirthDay.toDateString();
   }
 
-  checkLeapYear(year) {
+  checkLeapYear(year: number) {
     return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+  }
+
+  validateNic(nicNo: any): boolean {
+    if (nicNo.length === 10) {
+      return (
+        !isNaN(nicNo.substr(0, 9)) &&
+        isNaN(nicNo.substr(9, 1)) &&
+        ["x", "v"].includes(nicNo.substr(9, 1).toLowerCase())
+      );
+    } else {
+      return nicNo.length === 12 && !isNaN(nicNo);
+    }
   }
 }
